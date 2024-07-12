5598d1bdefd85d684aef9bbed71e66e61bc2bad0
Md Sirajul Basir <mohammadsirajulbasir@gmail.com>
2024-07-12 20:05:53 +0530

    Update build_funcs.py

--- build_funcs.py
+++ build_funcs.py
@@ -19,7 +19,6 @@ void parallelize(int Nthreads);
 void read_fof(char *fname, int read_flag, long *totcluster, float *halo, float *aa);
 void get_nhs(float *nh, float *ng, float *nx,long N1, long N2, long N3,float LL, float nion, float rmfp);
 void density_2_mass(float *ro_dum,float *data,long MM, long N1,long N2,long N3,int xin, int yin, int zin, int col, float *mass);
-void calpow_mom(float *ro,int Nbin,double* power, double* kmode, double* power_P2,double* power_P4, double *no, long N1, long N2, long N3, float LL);
 """)
 
 if __name__ == "__main__":
