89f306cd4a24424c2f14a79a31d1c235085bb959
Md Sirajul Basir <mohammadsirajulbasir@gmail.com>
2024-07-12 20:06:22 +0530

    Update source.h

--- source.h
+++ source.h
@@ -6,4 +6,3 @@ void get_nhs(float *nh, float *ng, float *nx,long N1, long N2, long N3,float LL,
 void density_2_mass(float *ro_dum,float *data,long MM, long N1,long N2,long N3,int xin, int yin, int zin, int col, float *mass);
 float  ***allocate_fftwf_3d(long N1,long N2,long N3);
 void deallocate_3d_double(float ***array, long N1, long N2);
-void calpow_mom(float *ro,int Nbin,double* power, double* kmode, double* power_P2,double* power_P4, double *no, long N1, long N2, long N3, float LL);
