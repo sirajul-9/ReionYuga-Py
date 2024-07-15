3df94dff672c8e668456e6014a8f68d6ce4dbd30
Md Sirajul Basir <mohammadsirajulbasir@gmail.com>
2024-07-15 12:35:04 +0530

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -20,12 +20,53 @@ class nbody_info:
         self.tot_DM=tot_DM
         self.DM_mass=DM_m
 
+#function for setting number of OpenMP threads
 def set_num_threads(nthreads):
+    """
+    sets the number of OpenMP threads to nthreads for shared memory parllelism
+
+    Parameters
+    ----------
+    nthreads: int
+             Number of OpenMP threads
+    """
     n=ffi.cast("int", nthreads)
     lib.parallelize(n)
 
+#function for reading grid spacing,N1,N2,N3, total number of dark matter particles etc
 def get_box_info(fname):
-    # Convert Python objects to CFFI objects
+    """
+    Reads the grid dimensions, total dark matter, and other necessary parameters from a specified N-body simulation file and returns an instance of the `nbody_info` class.
+    
+    Parameters
+    ----------
+    fname : str
+        The filename of the N-body data file.
+    
+    Returns
+    -------
+    nbody_info
+        An object of the `nbody_info` class containing:
+        - grid : grid_info
+            The computational grid information(dimensions and spacing).
+        - params : density_params
+            The cosmological density parameters.
+        - scale_factor : float
+            The scale factor of cosmic expansion.
+        - tot_DM : int
+            The total number of dark matter particles.
+        - DM_mass : float
+            The mass of individual dark matter particles.
+    
+    Description
+    -----------
+    The `get_box_info` function reads data from a specified N-body output file. It extracts the grid dimensions (`N1, N2, N3`), grid-spacing, total dark matter particles (`tot_DM`), and other parameters as written above.
+    
+    Examples
+    --------
+    >>> filename = "output.nbody_8.000"
+    >>> nbody_data = get_box_info(filename)
+    """
     fname_c = ffi.new("char[]", fname.encode('utf-8'))
     box=np.zeros(4, dtype=np.int64)
     la = np.zeros(7, dtype=np.float32)
@@ -35,7 +76,7 @@ def get_box_info(fname):
     #vva_c = ffi.cast("float*", vva.ctypes.data)
     la_c = ffi.cast("float*", la.ctypes.data)
 
-    # Call the C function
+    
     lib.read_output(fname_c, 1, box_c, dummy, dummy, la_c)
     
     p = density_params(np.float32(la[1]), np.float32(la[2]), np.float32(la[3]))
@@ -43,8 +84,39 @@ def get_box_info(fname):
     obj = nbody_info(g, p, np.float32(la[6]), np.int64(box[3]), np.float32(la[5]))
     return obj
 
-
+#function for reading position and velocity of dark matter particles
 def read_nbody_output(fname):
+    """
+    Reads the position, velocity of dark matter particles from an N-body simulation output file.
+
+    Parameters
+    ----------
+    fname : str
+        The filename of the N-body output data file.
+
+    Returns
+    -------
+    list
+        A list containing the position and velocity arrays:
+        - [position, velocity]
+            - position : numpy.ndarray
+                The position array of shape (total_particles, 3).
+            - velocity : numpy.ndarray
+                The velocity array of shape (total_particles, 3).
+
+    Description
+    -----------
+    The `read_nbody_output` function reads the position and velocity data from the N-body simulation output file.
+
+    Examples
+    --------
+    >>> filename = "output.nbody_8.000"
+    >>> position, velocity = read_nbody_output(filename)
+    >>> print(position.shape)
+    (1000, 3)
+    >>> print(velocity.shape)
+    (1000, 3)
+    """
     fname_c = ffi.new("char[]", fname.encode('utf-8'))
     obj = get_box_info(fname)
     
@@ -61,14 +133,14 @@ def read_nbody_output(fname):
     lib.read_output(fname_c,2,box_c,r_ptr,v_ptr,la_c)
     return [rra,vva]  
 
-#function which returns tot_cluster , halo and vaa
+#function which returns halo mass, position and velocity
 def read_halo_catalogue(fname):
     """
     Reads the halo catalogue data from a specified file.
 
     Parameters
     ----------
-    filename : bytes
+    filename : str
         The filename of the halo catalogue data file.
 
     Returns
@@ -85,7 +157,7 @@ def read_halo_catalogue(fname):
 
     Examples
     --------
-    >>> filename = "halo_catalogue.dat"
+    >>> filename = "halo_catalogue_8.000"
     >>> halo_catalogue = read_halo_catalogue(filename)
     >>> print(halo_catalogue.shape)
     (1000, 7)
@@ -141,10 +213,6 @@ def cic_vmass(data,out_arr_dims,xindex,yindex,zindex,mindex):
         The dimensions of the output array (N1, N2, N3).
     xindex, yindex, zindex, mindex : int
         Indices for accessing the particle positions and masses in the input dataset.
-    run_parallel : bool, optional
-        If True, the computation will run in parallel. Defaults to False.
-    num_threads : int, optional
-        Number of threads to use for parallel computation. Defaults to 1.
 
     Returns
     -------
@@ -153,12 +221,8 @@ def cic_vmass(data,out_arr_dims,xindex,yindex,zindex,mindex):
 
     Description
     -----------
-    The cic_vmass function computes the Cloud-in-Cell (CIC) Voronoi mass assignment for a given dataset. 
-    It initializes an output array ro with dimensions specified by out_arr_dims. 
-    The function then calculates the total number of particles and the number of columns in the input data. 
-    If run_parallel is True, the computation is performed in parallel using the specified number of threads (num_threads). 
-    Otherwise, the computation runs serially. The output array ro is rounded to 6 decimal places before being returned.
-
+    The cic_vmass function computes the Cloud-in-Cell (CIC) mass assignment for a given dataset. 
+    
     Examples
     --------
     >>> data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
@@ -180,16 +244,16 @@ def cic_vmass(data,out_arr_dims,xindex,yindex,zindex,mindex):
 
 def get_ion(nh,ngamma,grid_spacing,nion,rmfp):
     """
-    Calculates the ionization density based on given parameters.
+    Calculates the ionization density based on given parameters. 
 
     Parameters
     ----------
     nh : numpy.ndarray
         The neutral hydrogen density array of shape (N1, N2, N3).
     ngamma : numpy.ndarray
-        The ionizing photon density array of shape (N1, N2, N3).
+        The halo density array of shape (N1, N2, N3).
     grid_spacing : float
-        The grid spacing parameter.
+        The grid spacing 
     nion : float
         Parameter N_ion.
     rmfp : float
@@ -203,12 +267,13 @@ def get_ion(nh,ngamma,grid_spacing,nion,rmfp):
     Description
     -----------
     The get_ion function calculates the ionization density based on the given neutral hydrogen density (nh),
-    ionizing photon density (ngamma)and the reionization model parameters (nion and rmfp). See documentation for more info.
+    ionizing photon density (ngamma)and the reionization model parameters (nion and rmfp).It implements the Excursion Set Formalism.
+    See documentation for more info.
 
     Examples
     --------
-    >>> nh = np.random.rand(3, 3, 3) * 10  # Random neutral hydrogen density
-    >>> ngamma = np.random.rand(3, 3, 3) * 5  # Random ionizing photon density
+    >>> nh = np.random.rand(3, 3, 3) * 10  # Random hydrogen density
+    >>> ngamma = np.random.rand(3, 3, 3) * 5  # Random halo density
     >>> grid_spacing = 0.1
     >>> nion = 0.5
     >>> rmfp = 1.0
